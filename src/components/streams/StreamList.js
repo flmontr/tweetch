5c24610cee5e7fbd44ed0411bc58e2ed15f5a3df
Felipe Monteiro <felipe.rodrigues@superlogica.com>
2020-06-29 21:49:28 -0300

    StreamList setup

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,11 +1,74 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 
-const StreamList = () => {
-  return(
-    <div className="">
-      StreamList
-    </div>
-  )
+import {fetchStreams} from '../../actions'
+
+class StreamList extends React.Component {
+
+  componentDidMount = () => {
+    this.props.fetchStreams()
+  }
+
+  renderButtons = stream => {
+    if(stream.userId === this.props.currentUserId){
+      return(
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      )
+    }
+  }
+
+  renderList = () => {
+    return this.props.streams.map(stream => {
+      return(
+        <div className="item" key={stream.id}>
+          {this.renderButtons(stream)}
+          <i className="large middle aligned icon camera"></i>
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  renderCreateButton = () => {
+    if(this.props.signedIn){
+      return(
+        <div style={{textAlign: 'right'}}>
+          <Link to='/streams/new' className='ui button primary'>
+            Create stream
+          </Link>
+        </div>
+      )
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreateButton()}
+      </div>
+    )
+  }
 }
 
-export default StreamList
+const mapStateToProps = ({streams, auth}) => {
+  return {
+    streams: Object.values(streams),
+    currentUserId: auth.userId,
+    signedIn: auth.signedIn
+  }
+  /*
+  Transforming object to array to map over it. Streams is kept as
+  an object in state since itś easier to manipulate it inside reducer
+  */
+}
+
+export default connect(mapStateToProps, {fetchStreams})(StreamList)
