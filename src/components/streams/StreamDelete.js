e8e77049a717c8ed82872c343cf2a38da8f3556b
Felipe Monteiro <felipe.rodrigues@superlogica.com>
2020-07-04 12:23:31 -0300

    modal

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Modal from '../modal'
+import history from '../../history'
+
 
 const StreamDelete = () => {
+
+  const actions = (
+    <>
+      <div className="button ui negative">Delete</div>
+      <div className="button ui">Cancel</div>
+    </>
+  )
+  
   return(
     <div className="">
       StreamDelete
+      <Modal 
+        title='Delete Stream'
+        content='Are you sure?'
+        actions={actions}
+        onDismiss={() => history.push('/')}
+      />
     </div>
   )
 }
